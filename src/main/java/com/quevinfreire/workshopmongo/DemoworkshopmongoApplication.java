ea925dcf8b26ae6b5a7cdd52e0f6ba0b442dc6aa
Quevin Freire <quevinfreire93@gmail.com>
2022-11-29 03:21:35 -0400

    My first commit

---
+++ src/main/java/com/quevinfreire/workshopmongo/DemoworkshopmongoApplication.java
@@ -0,0 +1,13 @@
+package com.quevinfreire.workshopmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoworkshopmongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoworkshopmongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/quevinfreire/workshopmongo/DemoworkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quevinfreire.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoworkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
