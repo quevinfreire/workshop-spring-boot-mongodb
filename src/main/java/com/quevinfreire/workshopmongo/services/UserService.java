76da8b3286d89d3b1a40554cf9f2ae55b5472d90
Quevin Freire <quevinfreire93@gmail.com>
2022-11-29 04:06:21 -0400

    Connecting MongoDB repository and service

---
+++ src/main/java/com/quevinfreire/workshopmongo/services/UserService.java
@@ -0,0 +1,20 @@
+package com.quevinfreire.workshopmongo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.quevinfreire.workshopmongo.domain.User;
+import com.quevinfreire.workshopmongo.repository.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repo;
+	
+	public List<User> findAll(){
+		return repo.findAll();
+	}
+}
