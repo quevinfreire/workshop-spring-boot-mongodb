7f954d05d18f5aa879899d9bdaecfcd2563ee7ad
Quevin Freire <quevinfreire93@gmail.com>
2022-12-01 04:42:08 -0400

    Simple query with query methods

--- src/main/java/com/quevinfreire/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/quevinfreire/workshopmongo/repository/PostRepository.java
@@ -1,5 +1,7 @@
 package com.quevinfreire.workshopmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,7 @@ import com.quevinfreire.workshopmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
 	
+	//Query method 
+	List<Post> findByTitleContainingIgnoreCase(String text);
+		
 }
